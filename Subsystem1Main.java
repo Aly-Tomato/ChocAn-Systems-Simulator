7abfc8ee9d23f612e154b3c2de42c8ce4740d0c9
Alyssa Tamayo <atamayo@pdx.edu>
2018-11-26 19:20:05 -0800

    prompt for comments and create date. Ready for ya Victor!

--- Subsystem1Main.java
+++ Subsystem1Main.java
@@ -1,5 +1,5 @@
 /**
- * Created by Victor on 11/15/2018.
+ * Created by Victor and Alyssa on 11/15/2018.
  */
 import java.util.Scanner;
 import java.io.File;
@@ -10,10 +10,23 @@ import java.io.IOException;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 public class Subsystem1Main {
 
     public static void main(Scanner inputScanner) {
+        //data members
         memberServices obj = new memberServices();
+        int providerID;
+        int serviceID;
+        boolean rc; //return code
+        int memberID;
+        String comments;
+        DateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
+        Date date = new Date();
+        
 
         String memberFileLocation = new String();
         String providerFileLocation = new String();
@@ -24,12 +37,12 @@ public class Subsystem1Main {
         System.out.println("\n\nHello Provider,");
         System.out.println("-Please enter your 9 digit provider number-");
         System.out.println("ID Number: ");
-        int providerID = getInt(inputScanner); //Gets input from User
+        providerID = getInt(inputScanner); //Gets input from User
         System.out.println("-Please enter your 6 digit service number-");
-        int service = getInt(inputScanner); //Gets input from User
+        serviceID = getInt(inputScanner); //Gets input from User
 
         //validates provider login
-        boolean rc = obj.isValid(providerID, memberFileLocation);
+        rc = obj.isValid(providerID, memberFileLocation);
         if(rc){
           System.out.println("Provider Number is VALID");
         }
@@ -37,12 +50,11 @@ public class Subsystem1Main {
           System.out.println("Provider Number is INVALID. Goodbye");
           return; 
         }
-        
 
         //member validation
         System.out.println("-Please enter the 9 digit member number-");
         System.out.println("ID Number: ");
-        int memberID = getInt(inputScanner); //Gets input from User
+        memberID = getInt(inputScanner); //Gets input from User
 
         //validates member login
         rc = obj.isValid(memberID, memberFileLocation);
@@ -54,6 +66,7 @@ public class Subsystem1Main {
           return; 
         }
 
+        //check if member is supended
         rc = obj.isSuspended(memberID, memberFileLocation);
         if(rc){
           System.out.println("Member is Suspended. Please refer patient to billing services");
@@ -63,9 +76,19 @@ public class Subsystem1Main {
           System.out.println("Member is NOT Suspended. Proceed with services.");
         }
 
+        //provider enters comments about visit with member
+        System.out.println("\n\nWould you like to write comments about this visit?\n1 - yes\n2 - no\n");
+        int response = getInt(inputScanner);
+        if(response == 1){
+          System.out.println("\nPlease type your comments: \n");
+          comments = new String();
+        }
+        else{
+          System.out.println("\nYou answered no.\n");
+        }
+        
 
-
-        obj.writeReport(providerID, service, memberFileLocation);
+        obj.writeReport(providerID, serviceID, memberFileLocation);
 
         /*else {
             System.out.println("The Provider ID number you have entered is incorrect.");
@@ -85,6 +108,7 @@ public class Subsystem1Main {
     private static int getInt(Scanner inputScanner) {
         int return_this;
         return_this = inputScanner.nextInt();
+        inputScanner.nextLine();
         return return_this;
     }
     }
